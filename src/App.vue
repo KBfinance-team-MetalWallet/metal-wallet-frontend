9d8a22149d24d3e41ba7ce2716dc455a89b6bc2c
seowonn <sonseowon5@gmail.com>
2024-09-13 17:13:33 +0900

    Remove: 초기 설정 파일 제거

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
@@ -12,7 +11,6 @@ import HelloWorld from './components/HelloWorld.vue'
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
   </header>
