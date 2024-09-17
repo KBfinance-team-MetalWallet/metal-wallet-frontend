04a6993d6b6e9de74519419be4ff2897f3db7948
chuseok <90127lucy@gmail.com>
2024-09-18 00:45:26 +0900

    Refactor: 디자인 수정 및 Vue 컴포넌트 분리
    - 나의 예매 내역 페이지 피그마 그룹화
    - 컴포넌트 분리: Header, MainContent, Footer

---
+++ src/components/mypages/Header.vue
@@ -0,0 +1,47 @@
+<template>
+    <div :class="$style.weuibackFilledParent">
+        <img :class="$style.weuibackFilledIcon" alt="" :src="backIcon" />
+        <b :class="$style.b">뒤로가기</b>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import backIcon from '@/assets/mypages/backIcon.svg';
+
+export default defineComponent({
+    name: 'Header',
+    data() {
+        return {
+            backIcon,
+        };
+    },
+});
+</script>
+
+<style module>
+.weuibackFilledIcon {
+    position: absolute;
+    top: 2px;
+    left: 0px;
+    width: 11px;
+    height: 22px;
+    overflow: hidden;
+}
+
+.b {
+    position: absolute;
+    top: 2px;
+    left: 18px;
+}
+
+.weuibackFilledParent {
+    position: absolute;
+    top: 12px;
+    left: 14px;
+    width: 82px;
+    height: 22px;
+    font-size: 16px;
+    color: #6e6e6e;
+}
+</style>
---
+++ src/pages/mypages/MyTicketList.vue
@@ -0,0 +1,81 @@
+<template>
+    <div :class="$style.div">
+        <Header />
+        <MainContent :tickets="tickets" />
+        <Footer />
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Header from '@/components/mypages/Header.vue';
+import MainContent from '@/components/mypages/MainContent.vue';
+import Footer from '@/components/mypages/Footer.vue';
+import image200 from '@/assets/mypages/image 200.png'; // 이미지 파일 import
+
+export default defineComponent({
+    name: 'MyTicketList',
+    components: {
+        Header,
+        MainContent,
+        Footer
+    },
+    data() {
+        return {
+            tickets: [
+                {
+                    id: 'T200098371820231205',
+                    title: '뮤지컬 <알라딘> 한국 초연 (ALLADDIN The Musical) - 2매',
+                    date: '2023.12.05',
+                    venue: '계명아트센터',
+                    time: '2024.01.02(화) 19:30',
+                    seats: 'R석 11열 6, R석 11열 7',
+                    cancelBy: '2024.01.01(월) 17:00 까지',
+                    status: '예매완료',
+                    image: image200,
+                },
+                {
+                    id: 'T200098371820231204',
+                    title: '뮤지컬 <알라딘> 한국 초연 (ALLADDIN The Musical) - 2매',
+                    date: '2023.12.03',
+                    venue: '계명아트센터',
+                    time: '2024.01.02(화) 19:30',
+                    seats: 'R석 11열 6',
+                    cancelBy: '2024.01.01(월) 17:00',
+                    status: '교환신청',
+                    image: image200,
+                },
+                {
+                    id: 'T200098371820231203',
+                    title: '뮤지컬 <알라딘> 한국 초연 (ALLADDIN The Musical) - 2매',
+                    date: '2023.12.04',
+                    venue: '계명아트센터',
+                    time: '2024.01.02(화) 19:30',
+                    seats: 'R석 11열 6, R석 11열 7',
+                    cancelBy: '2024.01.01(월) 17:00 까지',
+                    status: '취소됨',
+                    image: image200,
+                },
+            ]
+        };
+    }
+});
+</script>
+
+<style module>
+body {
+    margin: 0;
+}
+
+.div {
+    width: 100%;
+    position: relative;
+    background-color: #fafafa;
+    height: 812px;
+    overflow: hidden;
+    text-align: left;
+    font-size: 10px;
+    color: #000;
+    font-family: Roboto;
+}
+</style>
