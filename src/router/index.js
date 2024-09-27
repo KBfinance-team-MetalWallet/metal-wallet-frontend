b82e2c5d2f502d20b0bc8b96481e6ce6fc492dc6
JunR <68640939+lee-JunR@users.noreply.github.com>
2024-09-27 14:11:11 +0900

    Feat : 뮤지컬 좌석 선택 기능 구현
    - 템플릿에서 좌석 레이아웃을 직접 구성하고 좌석 선택 기능 구현

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,7 @@ import MyTicketList from "@/pages/mypages/MyTicketList.vue";
 import Admin from "@/pages/admin/Admin.vue";
 import Camera from "@/pages/admin/QRCamera.vue";
 import AdminTicketList from "@/pages/admin/AdminTicketList.vue";
+import SeatSelectionPage from "@/pages/booking/SeatSelectionPage.vue";
 
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
@@ -27,6 +28,11 @@ const router = createRouter({
 			name: "login",
 			redirect: "/login/signup",
 			children: [
+				{
+					path: "login",
+					name: "login",
+					component: LoginForm,
+				},
 				{
 					path: "signup",
 					name: "signup",
@@ -95,6 +101,18 @@ const router = createRouter({
 					path: "ticketList",
 					name: "AdminTicketList",
 					component: AdminTicketList,
+				},
+			],
+		},
+		{
+			path: "/booking",
+			name: "Booking",
+			redirect: "/booking/seats",
+			children: [
+				{
+					path: "seats",
+					name: "seats",
+					component: SeatSelectionPage,
 				},
 			],
 		},
