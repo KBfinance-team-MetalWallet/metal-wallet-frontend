7b65df7642e8cc3d98c8519989775d6d27a5629f
chuseok <90127lucy@gmail.com>
2024-10-02 17:57:03 +0900

    Feat: 홈화면 계좌목록조회 api 연동

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -3,7 +3,7 @@
 		<div :class="$style.groupParent">
 			<MainHeader />
 			<!-- Use FlipAccount Component -->
-			<FlipAccount />
+			<FlipAccount :accounts="accountStore.accounts" v-if="!accountStore.isLoading" />
 			<!-- Musical Top 10 Section Component -->
 			<MusicalTop10 />
 		</div>
@@ -13,48 +13,56 @@
 </template>
 
 <script>
-	import Footer from "@/components/Footer.vue";
-	import MainHeader from "@/components/MainHeader.vue";
-	import FlipAccount from "@/components/home/FlipAccount.vue";
-	import MusicalTop10 from "@/components/home/MusicalTop10.vue";
+import Footer from "@/components/Footer.vue";
+import MainHeader from "@/components/MainHeader.vue";
+import FlipAccount from "@/components/home/FlipAccount.vue";
+import MusicalTop10 from "@/components/home/MusicalTop10.vue";
+import { defineComponent } from "vue";
+import { useAccountStore } from "@/stores/accounts";
 
-	import { defineComponent } from "vue";
+export default defineComponent({
+	name: "Home",
+	components: {
+		Footer,
+		MainHeader,
+		FlipAccount,
+		MusicalTop10,
+	},
+	setup() {
+		const accountStore = useAccountStore(); // Account Store 사용
+		accountStore.fetchAccounts(); // 계좌 목록을 가져옵니다.
 
-	export default defineComponent({
-		name: "Home",
-		components: {
-			Footer,
-			MainHeader,
-			FlipAccount,
-			MusicalTop10,
-		},
-	});
+		return {
+			accountStore, // Store를 반환하여 템플릿에서 사용 가능하게 합니다.
+		};
+	},
+});
 </script>
 <style module>
-	@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
 
-	body {
-		margin: 0;
-		line-height: normal;
-	}
+body {
+	margin: 0;
+	line-height: normal;
+}
 
-	.groupParent {
-		position: relative;
-		width: 100%;
-		padding: 14px;
-	}
+.groupParent {
+	position: relative;
+	width: 100%;
+	padding: 14px;
+}
 
-	.div {
-		width: 100%;
-		position: relative;
-		background-color: #fafafa;
-		min-height: 100vh;
-		overflow: hidden;
-		text-align: left;
-		font-size: 16px;
-		color: #c54966;
-		font-family: Roboto;
-	}
+.div {
+	width: 100%;
+	position: relative;
+	background-color: #fafafa;
+	min-height: 100vh;
+	overflow: hidden;
+	text-align: left;
+	font-size: 16px;
+	color: #c54966;
+	font-family: Roboto;
+}
 
-	/* 필요한 추가 스타일이 있다면 여기에 작성하세요 */
+/* 필요한 추가 스타일이 있다면 여기에 작성하세요 */
 </style>
