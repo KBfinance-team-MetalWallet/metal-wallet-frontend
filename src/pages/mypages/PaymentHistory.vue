12049641ae9e425534a858fc98f87905a4258997
seowonn <sonseowon5@gmail.com>
2024-10-03 17:49:16 +0900

    Feat: 단일 계좌 결제 내역 조회 페이지

--- src/pages/mypages/PaymentHistory.vue
+++ src/pages/mypages/PaymentHistory.vue
@@ -1,410 +1,232 @@
 <template>
-  <div :class="$style.div">
-      <MainHeader />
-      <div :class="$style.groupParent">
-          <div :class="$style.horizontalborderWrapper">
-              <div :class="$style.horizontalborder1">
-                  <b :class="$style.label">이메일</b>
-                  <input v-model="emailPrefix" type="email" :class="$style.input" placeholder="이메일을 입력해주세요." />
-                  <select v-model="selectedEmailDomain" :class="$style.select" @change="updateEmail">
-                      <option value="">직접입력</option>
-                      <option v-for="emailDomain in predefinedEmails" :key="emailDomain" :value="emailDomain">
-                          {{ emailDomain }}
-                      </option>
-                  </select>
-              </div>
-          </div>
-          <div :class="$style.horizontalborderWrapper">
-              <div :class="$style.horizontalborder1">
-                  <b :class="$style.label">비밀번호</b>
-                  <input v-model="formData.password" type="password" :class="$style.input1"
-                      placeholder="비밀번호를 입력해주세요." />
-              </div>
-          </div>
+  <div :class="$style.container">
+    <MainHeader class="main-header" />
+    <div :class="$style.content">
+      <div :class="$style.inner">
+        <b :class="$style.pageName">결제 내역</b>
       </div>
-      <div :class="$style.button1" :style="{ backgroundColor: isFormValid ? '#C54966' : '#CCCCCC' }"
-          :disabled="!isFormValid" @click="login">
-          <b :class="$style.b1">로그인</b>
+      <div :class="$style.child" />
+
+      <!-- 계좌 정보 카드 -->
+      <div :class="$style.rectangleIcon">
+        <div :class="$style.balanceAndImage">
+          <div class="account-info">
+            <b :class="$style.kb">KB나라사랑우대통장</b>
+            <div :class="$style.div1">123-4444-5555</div>
+          </div>
+
+          <!-- 금액 및 이미지 -->
+          <img :class="$style.image141Icon" alt="" src="@/assets/mypages/kbImage.png" />
+        </div>
+        <b :class="$style.balance">5,212,500원</b>
+
+        <!-- 이체하기 버튼 -->
+        <div :class="$style.button">
+          <div :class="$style.buttonText">이체하기</div>
+        </div>
       </div>
+
+      <!-- 거래 내역 리스트 컴포넌트 -->
+      <TransactionRecords :transactions="transactions" />
+    </div>
+    <!-- Footer -->
+    <Footer class="footer" />
   </div>
 </template>
-<script lang="ts">
-import { defineComponent } from 'vue'
-import MainHeader from '../../components/MainHeader.vue';
-import axios from 'axios';
 
+<script lang="js">
+import { defineComponent } from 'vue';
+import axios from 'axios';
+import MainHeader from '../../components/MainHeader.vue';
+import Footer from '../../components/Footer.vue';
+import TransactionRecords from '../../components/mypages/TransactionRecords.vue'; // 추가
 
 export default defineComponent({
-  name: "SignupForm",
+  name: "Frame",
   components: {
-      MainHeader,
+    MainHeader,
+    Footer,
+    TransactionRecords, // 등록
   },
   data() {
-      return {
-          emailPrefix: '',
-          selectedEmailDomain: '',
-          formData: {
-              email: '',
-              password: ''
-          },
-          predefinedEmails: [
-              '@gmail.com',
-              '@naver.com',
-              '@daum.net'
-          ]
-      }
+    return {
+      transactions: [],
+      accountId: 8,
+    };
   },
-  watch: {
-      emailPrefix() {
-          this.updateEmail();
-      },
-      selectedEmailDomain() {
-          this.updateEmail();
-      }
+  mounted() {
+    this.fetchTransactionRecords();
   },
   methods: {
-      updateEmail() {
-          if (this.selectedEmailDomain) {
-              this.formData.email = `${this.emailPrefix}${this.selectedEmailDomain}`;
-          } else {
-              this.formData.email = this.emailPrefix;
+    async fetchTransactionRecords() {
+      const token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZXN0QGdtYWlsLmNvbSIsImV4cCI6MTcyODAyNTA0OCwicm9sZSI6IlVTRVIifQ.VkHPsFkYvpWDAbySASPa52usMr1CyhZJLmR7C75niRIQxKz_hrv8bDnlXsu7ltmkOg48whrt5rmjmyupX-576w";
+      try {
+        const response = await axios.get(`http://localhost:8080/api/accounts/${this.accountId}/transaction-records`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
           }
-      },
-      async login() {
-          if (!this.isFormValid) {
-              alert('이메일과 비밀번호를 입력해주세요.');
-              return;
-          }
-          try {
-              const response = await axios.post('http://localhost:8080/api/members/login', this.formData);
-              console.log('Login successful:', response.data);
-
-              // accessToken 로컬스토리지에 저장
-              if (response.data.accessToken) {
-                  localStorage.setItem('accessToken', response.data.accessToken);
-              } else {
-                  console.error('Access token not found in response');
-              }
-          } catch (error) {
-              console.error('Error login member:', error.response.data);
-          }
+        );
+        this.transactions = response.data.result.data.map(transaction => ({
+          transactionId: transaction.transactionId,
+          vendor: transaction.vendor,
+          amount: transaction.amount,
+          transactionType: transaction.transactionType,
+          currentBalance: transaction.currentBalance,
+          createdAt: transaction.createdAt
+        }));
+      } catch (error) {
+        console.error("Error fetching transaction records:", error);
       }
-  },
-  computed: {
-      isFormValid() {
-          return (
-              this.formData.email &&
-              this.formData.password
-          );
-      }
+    },
   }
-})
+});
 </script>
+
 <style module>
-.solarwalletOutlineIcon {
-  position: absolute;
-  top: 11px;
-  left: 14px;
-  width: 24px;
-  height: 24px;
-  overflow: hidden;
-}
-
-.b {
-  position: absolute;
-  top: 14px;
-  left: 43px;
-  color: #6e6e6e;
-}
-
-.label {
-  position: absolute;
-  top: 10px;
-  left: 5px;
-  line-height: 30px;
-}
-
-.div1 {
-  position: absolute;
-  top: 10px;
-  left: 239px;
-  line-height: 30px;
-  color: #666;
-}
-
-.icon {
-  position: absolute;
-  height: 9.8%;
-  width: 2.86%;
-  top: 45.1%;
-  right: 1.71%;
-  bottom: 45.1%;
-  left: 95.43%;
-  max-width: 100%;
-  overflow: hidden;
-  max-height: 100%;
-}
-
-.horizontalborder {
-  position: absolute;
-  top: 0px;
-  left: 0px;
-  border-bottom: 1px solid #000;
-  box-sizing: border-box;
-  width: 350px;
-  height: 51px;
-}
-
-.horizontalborderWrapper {
-  width: 350px;
-  position: relative;
-  height: 51px;
-}
-
-.horizontalborder1 {
-  align-self: stretch;
-  position: relative;
-  border-bottom: 1px solid #000;
-  box-sizing: border-box;
-  height: 51px;
-  font-size: 14.18px;
-}
-
-.div2 {
-  position: absolute;
-  top: 0px;
-  left: 0px;
-}
-
 .container {
-  position: absolute;
-  width: calc(100% - 35px);
-  top: 6px;
-  right: 25px;
-  left: 10px;
-  height: 18px;
-  overflow: hidden;
-}
-
-.input {
-  position: absolute;
-  width: calc(100% - 240px);
-  top: 10px;
-  right: 127px;
-  left: 113px;
-  height: 30px;
-  overflow: hidden;
-  font-size: 14px;
-  color: #000;
-  background-color: transparent;
-  border: none;
-  outline: none;
-}
-
-.input::placeholder {
-  font-size: 12px;
-  color: #999;
-  opacity: 1;
-}
-
-.div3 {
-  position: absolute;
-  top: 7px;
-  left: 14px;
-}
-
-.button {
-  position: absolute;
-  top: 10px;
-  left: 245px;
-  border-radius: 6px;
-  background-color: #fff;
-  border: 1px solid #ccc;
-  box-sizing: border-box;
-  width: 100px;
-  height: 30px;
-  text-align: center;
-  font-size: 13px;
-}
-
-.horizontalborder2 {
-  align-self: stretch;
-  position: relative;
-  border-bottom: 1px solid #000;
-  box-sizing: border-box;
-  height: 51px;
-}
-
-.container1 {
-  position: absolute;
-  top: 6px;
-  right: 0px;
-  width: 166.1px;
-  height: 18px;
-  overflow: hidden;
-}
-
-.input1 {
-  position: absolute;
-  width: calc(100% - 123px);
-  top: 10px;
-  right: 10px;
-  left: 113px;
-  height: 30px;
-  overflow: hidden;
-  font-size: 14px;
-  color: #000;
-  background-color: transparent;
-  border: none;
-  outline: none;
-}
-
-.input1::placeholder {
-  font-size: 12px;
-  color: #999;
-  opacity: 1;
-}
-
-.groupParent {
-  position: absolute;
-  top: 76px;
-  left: 12px;
-  width: 350px;
   display: flex;
   flex-direction: column;
-  align-items: flex-start;
   justify-content: flex-start;
-  gap: 10px;
-  font-size: 14.06px;
-}
-
-.sms {
-  position: absolute;
-  top: 10px;
-  left: 39px;
-  line-height: 20px;
-}
-
-.container2 {
-  width: 18px;
-  position: relative;
-  border-radius: 2px;
-  background-color: #65558f;
-  height: 18px;
-  z-index: 0;
-}
-
-.checkSmallIcon {
-  width: 24px;
-  position: absolute;
-  margin: 0 !important;
-  top: calc(50% - 12px);
-  left: calc(50% - 12px);
-  height: 24px;
-  z-index: 1;
-}
-
-.stateLayer {
-  position: absolute;
-  top: 0px;
-  left: 0px;
-  border-radius: 100px;
-  display: flex;
-  flex-direction: row;
   align-items: center;
-  justify-content: center;
-  padding: 11px;
-}
-
-.smsParent {
-  position: absolute;
-  top: 530px;
-  left: 6px;
-  width: 329px;
-  height: 40px;
-  font-size: 12.39px;
-}
-
-.p {
-  margin: 0;
-}
-
-.div5 {
-  position: absolute;
-  top: 622px;
-  left: 12px;
-  font-size: 14.18px;
-  line-height: 22px;
-}
-
-.b1 {
-  position: absolute;
-  top: 15.5px;
-  left: calc(50% - 28px);
-}
-
-.button1 {
-  position: absolute;
-  width: calc(100% - 25px);
-  top: 689px;
-  right: 13px;
-  left: 12px;
-  border-radius: 10px;
-  background-color: #ccc;
-  height: 50px;
-  text-align: center;
-  font-size: 15px;
-  color: #fff;
-}
-
-.div7 {
-  position: absolute;
-  top: 3px;
-  left: 33px;
-  line-height: 20px;
-}
-
-.div6 {
-  position: absolute;
-  height: 60%;
-  top: 20%;
-  bottom: 20%;
-  left: 6px;
-  width: 120.8px;
-}
-
-.parent {
-  position: absolute;
-  height: 4.93%;
-  top: 69.95%;
-  bottom: 25.12%;
-  left: 6px;
-  width: 126.8px;
-  font-size: 12.29px;
-}
-
-.div {
   width: 100%;
+  height: 100vh;
   position: relative;
-  background-color: #fafafa;
-  height: 812px;
-  overflow: hidden;
-  text-align: left;
+  background-color: #f3f3f3;
+}
+
+.main-header {
+  width: 100%;
+  height: 60px;
+}
+
+.content {
+  flex: 1;
+  width: 100%;
+  padding-top: 60px;
+  overflow: auto;
+  padding-bottom: 70px;
+}
+
+.inner {
+  text-align: center;
+  width: 100%;
+}
+
+.pageName {
   font-size: 16px;
-  color: #000;
+  color: #6e6e6e;
+  font-weight: bold;
   font-family: Roboto;
 }
 
-.select {
-  position: absolute;
-  top: 10px;
-  right: 10px;
-  height: 30px;
-  overflow: hidden;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  background-color: #fff;
-  outline: none;
-  font-size: 15px;
+.kb {
+  font-size: 14px;
+  font-family: Roboto;
+  font-weight: bold;
+  color: black;
+}
+
+.div1 {
+  margin-top: 3px;
+  font-size: 13px;
+  font-family: Roboto;
+  color: black;
+  margin-bottom: 10px;
+}
+
+.rectangleIcon {
+  width: 85%;
+  position: relative;
+  margin: 0 auto;
+  margin-top: 30px;
+  border-radius: 15px;
+  padding: 25px;
+  height: auto;
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+}
+
+.balanceAndImage {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
+
+.balance {
+  font-size: 28px;
+  font-weight: bold;
+  color: #333;
+}
+
+.image141Icon {
+  border-radius: 50%;
+  width: 50px;
+  height: 50px;
+  object-fit: cover;
+}
+
+.button {
+  margin-top: 10px;
+  border-radius: 5px;
+  background-color: #999;
+  width: 100%;
+  height: 40px;
+  font-weight: bold;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  box-sizing: border-box;
+  color: #333;
+}
+
+.transactionsFrame {
+  width: 85%;
+  position: relative;
+  margin: 0 auto;
+  margin-top: 10px;
+  border-radius: 15px;
+  padding: 15px;
+}
+
+.transactionRow {
+  display: flex;
+  justify-content: space-between;
+  padding: 10px 0;
+  border-bottom: 1px solid #ddd;
+}
+
+.transactionItem,
+.transactionItemTitle {
+  text-align: left;
+  font-size: 16px;
+  color: #fff;
+}
+
+.transactionAmount {
+  font-size: 16px;
+  color: #4f6af6;
+}
+
+.transactionBalance {
+  font-size: 12px;
+  color: #aaa;
+}
+
+/* Footer 하단 고정 */
+.footer {
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  height: 60px;
+  background-color: #222;
+  z-index: 10;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
 }
 </style>
