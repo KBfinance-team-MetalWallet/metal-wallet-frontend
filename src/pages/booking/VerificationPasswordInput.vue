947deba77939169b80489ddc0666937b6fa0a2a7
leehyeonhee <2hyeonheedev@gmail.com>
2024-10-08 00:14:20 +0900

    Feat: 티켓 생성 API 연결 완료

--- src/pages/booking/VerificationPasswordInput.vue
+++ src/pages/booking/VerificationPasswordInput.vue
@@ -1,265 +1,290 @@
 <template>
-	<div :class="$style.container">
-		<b :class="$style.title">
-			<p :class="$style.text">
-				<span>간편 비밀번호 </span>
-				<span :class="$style.highlight">를</span>
-			</p>
-			<p :class="$style.text">
-				<span :class="$style.highlight">{{
-					firstInput ? "입력" : "다시 한번 입력"
-				}}</span>
-				<span>해주세요.</span>
-			</p>
-		</b>
+  <div :class="$style.container">
+    <b :class="$style.title">
+      <p :class="$style.text">
+        <span>간편 비밀번호 </span>
+        <span :class="$style.highlight">를</span>
+      </p>
+      <p :class="$style.text">
+        <span :class="$style.highlight">{{
+          firstInput ? "입력" : "다시 한번 입력"
+        }}</span>
+        <span>해주세요.</span>
+      </p>
+    </b>
 
-		<div :class="$style.dividerwrapper">
-			<svg :class="$style.icon" xmlns="http://www.w3.org/2000/svg">
-				<g>
-					<circle
-						v-for="(circleColor, index) in circles"
-						:key="index"
-						:cx="15.5 + index * 43"
-						cy="15.5"
-						r="15.5"
-						:fill="circleColor"
-					/>
-				</g>
-			</svg>
-			<div :class="$style.divider"></div>
-		</div>
+    <div :class="$style.dividerwrapper">
+      <svg :class="$style.icon" xmlns="http://www.w3.org/2000/svg">
+        <g>
+          <circle
+            v-for="(circleColor, index) in circles"
+            :key="index"
+            :cx="15.5 + index * 43"
+            cy="15.5"
+            r="15.5"
+            :fill="circleColor"
+          />
+        </g>
+      </svg>
+      <div :class="$style.divider"></div>
+    </div>
 
-		<div :class="$style.keypad">
-			<div
-				v-for="(row, rowIndex) in keypadRows"
-				:key="rowIndex"
-				:class="$style.keypadRow"
-			>
-				<div
-					v-for="(key, keyIndex) in row"
-					:key="keyIndex"
-					:class="$style.key"
-					@click="handleKeyPress(key)"
-				>
-					<!-- 0을 제외한 숫자 렌더링 -->
-					<div
-						v-if="key !== null && key !== 'delete'"
-						:class="$style.numberRect"
-					></div>
-					<div v-if="key !== null && key !== 'delete'" :class="$style.number">
-						{{ key }}
-					</div>
+    <div :class="$style.keypad">
+      <div
+        v-for="(row, rowIndex) in keypadRows"
+        :key="rowIndex"
+        :class="$style.keypadRow"
+      >
+        <div
+          v-for="(key, keyIndex) in row"
+          :key="keyIndex"
+          :class="$style.key"
+          @click="handleKeyPress(key)"
+        >
+          <!-- 0을 제외한 숫자 렌더링 -->
+          <div
+            v-if="key !== null && key !== 'delete'"
+            :class="$style.numberRect"
+          ></div>
+          <div v-if="key !== null && key !== 'delete'" :class="$style.number">
+            {{ key }}
+          </div>
 
-					<!-- deleteIcon 렌더링 -->
-					<img
-						v-if="key === 'delete'"
-						:class="$style.deleteIcon"
-						alt="delete icon"
-						src="@/assets/login/deleteIcon.svg"
-					/>
+          <!-- deleteIcon 렌더링 -->
+          <img
+            v-if="key === 'delete'"
+            :class="$style.deleteIcon"
+            alt="delete icon"
+            src="@/assets/login/deleteIcon.svg"
+          />
 
-					<!-- 빈 공간 렌더링 -->
-					<div v-if="key === null" :class="$style.emptySpace"></div>
-				</div>
-			</div>
-		</div>
-	</div>
+          <!-- 빈 공간 렌더링 -->
+          <div v-if="key === null" :class="$style.emptySpace"></div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="js">
-			import axios from 'axios';
-	import { defineComponent } from 'vue';
+ import axios from "axios";
+ import { defineComponent } from "vue";
 
-			export default defineComponent({
-			    name: 'VerificationPasswordInput',
-			    props: {
-			        seats: {
-			            type: Array,
-			            required: true,
-			            default: () => [],
-			        },
-			    },
-			    data() {
-			        return {
-			            enteredPassword: [], // 입력된 비밀번호를 저장하는 배열
-			            maxPasswordLength: 6, // 최대 비밀번호 자리수
-			            firstInput: true, // 첫 번째 입력 여부
-			            firstPin: '', // 첫 번째 PIN 저장
-			            keypadRows: [
-			                [1, 2, 3],
-			                [4, 5, 6],
-			                [7, 8, 9],
-			                [null, 0, 'delete'],
-			            ],
-			        };
-			    },
-			    computed: {
-			        circles() {
-			            // 비밀번호 자리수에 맞춰 색상을 결정
-			            return Array.from({ length: this.maxPasswordLength }, (_, index) => {
-			                return this.enteredPassword.length > index ? '#C54966' : '#D9D9D9';
-			            });
-			        },
-			    },
-			    methods: {
-			        handleKeyPress(key) {
-			            if (typeof key === 'number' && this.enteredPassword.length < this.maxPasswordLength) {
-			                this.enteredPassword.push(key);
-			                // PIN 번호가 6자리가 모두 입력되었을 때
-			                if (this.enteredPassword.length === this.maxPasswordLength) {
-			                    if (this.firstInput) {
-			                        // 첫 번째 입력 시 PIN 저장
-			                        this.firstPin = this.enteredPassword.join('');
-			                        this.checkPassword();
-			                        this.enteredPassword = []; // 이전 비밀번호 초기화
-			                    }
-			                }
-			            } else if (key === 'delete' && this.enteredPassword.length > 0) {
-			                this.enteredPassword.pop();
-			            }
-			        },
-			        getDeviceInfo() {
-			            return navigator.userAgent;
-			        },
-			        async checkPassword() {
-			            const pinNumber = this.firstPin; // 현재 입력된 PIN 번호
-			            const deviceInfo = this.getDeviceInfo(); // 기기 정보 가져오기
+ export default defineComponent({
+name: "VerificationPasswordInput",
+data() {
+  return {
+	enteredPassword: [],
+	maxPasswordLength: 6,
+	firstInput: true,
+	firstPin: "",
+	keypadRows: [
+	  [1, 2, 3],
+	  [4, 5, 6],
+	  [7, 8, 9],
+	  [null, 0, "delete"],
+	],
+  };
+},
+computed: {
+  circles() {
+	return Array.from(
+	  { length: this.maxPasswordLength },
+	  (_, index) => {
+		return this.enteredPassword.length > index ? "#C54966" : "#D9D9D9";
+	  }
+	);
+  },
+  seatIds() {
+   try {
+     return JSON.parse(this.$route.query.seats || '[]');
+   } catch (error) {
+     console.error('Error parsing seatIds:', error);
+     return [];
+   }
+ },
+},
+methods: {
+  handleKeyPress(key) {
+	if (
+	  typeof key === "number" &&
+	  this.enteredPassword.length < this.maxPasswordLength
+	) {
+	  this.enteredPassword.push(key);
+	  if (this.enteredPassword.length === this.maxPasswordLength) {
+		if (this.firstInput) {
+		  this.firstPin = this.enteredPassword.join("");
+		  this.verifyPinAndBookTickets();
+		  this.enteredPassword = [];
+		}
+	  }
+	} else if (key === "delete" && this.enteredPassword.length > 0) {
+	  this.enteredPassword.pop();
+	}
+  },
+  getDeviceInfo() {
+	return navigator.userAgent;
+  },
+  async verifyPinAndBookTickets() {
+	const pinNumber = this.firstPin;
+	const deviceInfo = this.getDeviceInfo();
+	const token = localStorage.getItem("accessToken");
 
-			            // 서버로 전송할 데이터 준비
-			            const formData = {
-			                pinNumber, // 입력된 PIN 번호
-			                seats: this.seats, // 좌석 정보 전달
-			                deviceInfo, // 기기 정보 전달
-			            };
+	try {
+	  const response = await axios.post(
+		"http://localhost:8080/api/members/pin-number-verification",
+		{ pinNumber, deviceInfo },
+		{
+		  headers: {
+			Authorization: `Bearer ${token}`,
+		  },
+		}
+	  );
 
-			            // 로컬 스토리지에 저장된 JWT 토큰 가져오기
-			            const token = localStorage.getItem('accessToken');
+	  console.log("Verification successful:", response.data);
+	  await this.bookTickets();
+	  this.$router.push({ path: "/booking/payment-success" });
+	} catch (error) {
+	  console.error(
+		"Error during verification:",
+		error.response ? error.response.data : error.message
+	  );
+	}
+  },
+  async bookTickets() {
+	const formData = {
+	  seatId: this.seatIds,
+	  deviceId: this.deviceInfo ? this.deviceInfo : "temp",
+	};
+	const token = localStorage.getItem("accessToken");
 
-			            try {
-			                const response = await axios.post(
-			                    'http://localhost:8080/api/members/pin-number-verification',
-			                    formData,
-			                    {
-			                        headers: {
-			                            Authorization: `Bearer ${token}`,
-			                        },
-			                    }
-			                );
-			                console.log('Verification successful:', response.data);
-			                this.$router.push({ path: '/booking/payment-success' });
-			            } catch (error) {
-			                console.error('Error during verification:', error.response ? error.response.data : error.message);
-			            }
-			        },
-			    },
-			});
+	try {
+	  const response = await axios.post(
+		"http://localhost:8080/api/tickets",
+		formData,
+		{
+		  headers: {
+			Authorization: `Bearer ${token}`,
+		  },
+		}
+	  );
+	  console.log("Tickets booked successfully:", response.data);
+	} catch (error) {
+	  console.error(
+		"Error booking tickets:",
+		error.response ? error.response.data : error.message
+	  );
+	}
+  },
+},
+ });
 </script>
 
 <style module>
-	@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
 
-	body {
-		margin: 0;
-		line-height: normal;
-	}
+body {
+  margin: 0;
+  line-height: normal;
+}
 
-	.container {
-		width: 100%;
-		position: relative;
-		background-color: #fafafa;
-		height: 812px;
-		text-align: left;
-		font-family: Roboto, sans-serif;
-	}
+.container {
+  width: 100%;
+  position: relative;
+  background-color: #fafafa;
+  height: 812px;
+  text-align: left;
+  font-family: Roboto, sans-serif;
+}
 
-	.title {
-		position: absolute;
-		top: 107px;
-		left: 38px;
-		font-size: 24px;
-	}
+.title {
+  position: absolute;
+  top: 107px;
+  left: 38px;
+  font-size: 24px;
+}
 
-	.text {
-		margin: 0;
-		color: #6e6e6e;
-	}
+.text {
+  margin: 0;
+  color: #6e6e6e;
+}
 
-	.highlight {
-		color: #c54966;
-	}
+.highlight {
+  color: #c54966;
+}
 
-	.dividerwrapper {
-		position: absolute;
-		top: calc(50% - 149px);
-		left: calc(50% - 137.5px);
-		width: 276px;
-		height: 59px;
-	}
+.dividerwrapper {
+  position: absolute;
+  top: calc(50% - 149px);
+  left: calc(50% - 137.5px);
+  width: 276px;
+  height: 59px;
+}
 
-	.icon {
-		position: absolute;
-		top: calc(50% - 29.5px);
-		left: calc(50% - 123px);
-		width: 246px;
-		height: 31px;
-	}
+.icon {
+  position: absolute;
+  top: calc(50% - 29.5px);
+  left: calc(50% - 123px);
+  width: 246px;
+  height: 31px;
+}
 
-	.divider {
-		position: absolute;
-		top: calc(50% + 29px);
-		left: calc(50% - 138.5px);
-		border-top: 1px solid #000;
-		box-sizing: border-box;
-		width: 277px;
-		height: 1px;
-	}
+.divider {
+  position: absolute;
+  top: calc(50% + 29px);
+  left: calc(50% - 138.5px);
+  border-top: 1px solid #000;
+  box-sizing: border-box;
+  width: 277px;
+  height: 1px;
+}
 
-	.keypad {
-		position: absolute;
-		top: 501px;
-		left: 50px;
-		width: 281px;
-	}
+.keypad {
+  position: absolute;
+  top: 501px;
+  left: 50px;
+  width: 281px;
+}
 
-	.keypadRow {
-		display: flex;
-		flex-direction: row;
-		justify-content: space-between;
-		margin-bottom: 20px;
-	}
+.keypadRow {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin-bottom: 20px;
+}
 
-	.key {
-		width: 57px;
-		height: 38px;
-		position: relative;
-	}
+.key {
+  width: 57px;
+  height: 38px;
+  position: relative;
+}
 
-	.numberRect {
-		width: 100%;
-		height: 100%;
-		background-color: #d9d9d9;
-		opacity: 0;
-	}
+.numberRect {
+  width: 100%;
+  height: 100%;
+  background-color: #d9d9d9;
+  opacity: 0;
+}
 
-	.number {
-		position: absolute;
-		top: 10%;
-		left: 36%;
-		font-size: 25px;
-		color: #0b0b0b;
-	}
+.number {
+  position: absolute;
+  top: 10%;
+  left: 36%;
+  font-size: 25px;
+  color: #0b0b0b;
+}
 
-	.emptySpace {
-		position: absolute;
-		top: 10%;
-		left: 48%;
-	}
+.emptySpace {
+  position: absolute;
+  top: 10%;
+  left: 48%;
+}
 
-	.deleteIcon {
-		position: absolute;
-		top: 50%;
-		left: 50%;
-		transform: translate(-50%, -50%);
-		overflow: hidden;
-	}
+.deleteIcon {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  overflow: hidden;
+}
 </style>
