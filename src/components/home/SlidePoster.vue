1fb46b926b5a94b6808e9fcf908292fc1c406f03
chuseok <90127lucy@gmail.com>
2024-10-11 08:45:20 +0900

    Style: media css 제거

--- src/components/home/SlidePoster.vue
+++ src/components/home/SlidePoster.vue
@@ -1,17 +1,9 @@
 <template>
-	<div
-		class="slidePosterWrapper"
-		@mouseenter="pauseSlide"
-		@mouseleave="startSlide"
-	>
+	<div class="slidePosterWrapper" @mouseenter="pauseSlide" @mouseleave="startSlide">
 		<div class="bannerWrapper">
 			<div class="slideContainer" :style="slideStyle">
-				<div
-					class="slide"
-					v-for="(image, index) in images"
-					:key="index"
-					:style="{ width: `${100 / images.length}%` }"
-				>
+				<div class="slide" v-for="(image, index) in images" :key="index"
+					:style="{ width: `${100 / images.length}%` }">
 					<img :src="image" alt="배너 이미지" class="bannerImg" />
 				</div>
 			</div>
@@ -19,92 +11,85 @@
 	</div>
 </template>
 <script>
-	import kbBanner1 from "@/assets/slide/kbBanner1.jpg";
-	import kbBanner2 from "@/assets/slide/kbBanner2.jpg";
-	import kbBanner3 from "@/assets/slide/kbBanner3.jpg";
+import kbBanner1 from "@/assets/slide/kbBanner1.jpg";
+import kbBanner2 from "@/assets/slide/kbBanner2.jpg";
+import kbBanner3 from "@/assets/slide/kbBanner3.jpg";
 
-	export default {
-		name: "SlidePoster",
-		data() {
+export default {
+	name: "SlidePoster",
+	data() {
+		return {
+			currentIndex: 0,
+			images: [kbBanner1, kbBanner2, kbBanner3],
+			intervalId: null,
+		};
+	},
+	computed: {
+		slideStyle() {
 			return {
-				currentIndex: 0,
-				images: [kbBanner1, kbBanner2, kbBanner3],
-				intervalId: null,
+				transform: `translateX(-${this.currentIndex * (100 / this.images.length)
+					}%)`,
+				transition: "transform 0.5s ease-in-out",
+				width: `${this.images.length * 100}%`,
 			};
 		},
-		computed: {
-			slideStyle() {
-				return {
-					transform: `translateX(-${
-						this.currentIndex * (100 / this.images.length)
-					}%)`,
-					transition: "transform 0.5s ease-in-out",
-					width: `${this.images.length * 100}%`,
-				};
-			},
+	},
+	methods: {
+		nextSlide() {
+			this.currentIndex = (this.currentIndex + 1) % this.images.length;
 		},
-		methods: {
-			nextSlide() {
-				this.currentIndex = (this.currentIndex + 1) % this.images.length;
-			},
-			pauseSlide() {
-				clearInterval(this.intervalId);
-			},
-			startSlide() {
-				this.intervalId = setInterval(() => {
-					this.nextSlide();
-				}, 2000);
-			},
-		},
-		mounted() {
-			this.startSlide();
-		},
-		beforeUnmount() {
+		pauseSlide() {
 			clearInterval(this.intervalId);
 		},
-	};
+		startSlide() {
+			this.intervalId = setInterval(() => {
+				this.nextSlide();
+			}, 2000);
+		},
+	},
+	mounted() {
+		this.startSlide();
+	},
+	beforeUnmount() {
+		clearInterval(this.intervalId);
+	},
+};
 </script>
 <style scoped>
-	* {
-		box-sizing: border-box;
-	}
+* {
+	box-sizing: border-box;
+}
 
-	.slidePosterWrapper {
-		width: 100%;
-		height: 200px;
-		overflow: hidden;
-		position: relative;
-		margin-bottom: 150px;
-	}
+.slidePosterWrapper {
+	width: 100%;
+	height: 200px;
+	overflow: hidden;
+	position: relative;
+	margin-bottom: 150px;
+}
 
-	.bannerWrapper {
-		width: 100%;
-		height: 100%;
-		position: relative;
-	}
+.bannerWrapper {
+	width: 100%;
+	height: 100%;
+	position: relative;
+}
 
-	.slideContainer {
-		display: flex;
-		flex-wrap: nowrap;
-		height: 100%;
-		transition: transform 3s ease-in-out;
-	}
+.slideContainer {
+	display: flex;
+	flex-wrap: nowrap;
+	height: 100%;
+	transition: transform 3s ease-in-out;
+}
 
-	.slide {
-		flex: 0% 10% 0%;
-		height: 100%;
-	}
+.slide {
+	flex: 0% 10% 0%;
+	height: 100%;
+}
 
-	.bannerImg {
-		width: 375px;
-		height: 100%;
-		display: block;
-		object-fit: cover;
-	}
-
-	@media (max-width: 812px) {
-		.slidePosterWrapper {
-			height: 130px;
-		}
-	}
+.bannerImg {
+	width: 375px;
+	height: 100%;
+	display: block;
+	object-fit: cover;
+}
 </style>
