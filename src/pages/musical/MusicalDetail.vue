8d058976ebe78f6c74121a7db446a2336ccd47c1
chuseok <90127lucy@gmail.com>
2024-10-10 13:40:05 +0900

    Design: 예매 화면 CSS 폰트, 정렬 수정

--- src/pages/musical/MusicalDetail.vue
+++ src/pages/musical/MusicalDetail.vue
@@ -18,38 +18,22 @@
 			<!-- Content section with poster and musical details -->
 			<div :class="$style.content">
 				<div :class="$style.posterContainer">
-					<img
-						:class="$style.posterImage"
-						alt=""
-						:src="musical?.posterImage || TestImage"
-					/>
+					<img :class="$style.posterImage" alt="" :src="musical?.posterImage || TestImage" />
 					<div :class="$style.overlay">
 						<div :class="$style.details">
 							<b>
 								뮤지컬 &lt;{{ musical?.title }}&gt; - {{ musical?.place }}
 							</b>
 							<div>
-								<img
-									:class="$style.icoutlinePlaceIcon"
-									alt=""
-									:src="PlaceIcon"
-								/>
+								<img :class="$style.icoutlinePlaceIcon" alt="" :src="PlaceIcon" />
 								<span>{{ musical?.placeDetail }}</span>
 							</div>
 							<div>
-								<img
-									:class="$style.mdicalendarOutlineIcon"
-									alt=""
-									:src="CalendarIcon"
-								/>
+								<img :class="$style.mdicalendarOutlineIcon" alt="" :src="CalendarIcon" />
 								<span>{{ musical?.dates }}</span>
 							</div>
 							<div>
-								<img
-									:class="$style.mingcutetimeLineIcon"
-									alt=""
-									:src="TimeIcon"
-								/>
+								<img :class="$style.mingcutetimeLineIcon" alt="" :src="TimeIcon" />
 								<span>{{ musical?.runningTime }}</span>
 							</div>
 						</div>
@@ -60,36 +44,24 @@
 				<div :class="$style.sectionWrapper" ref="notice">
 					<div :class="$style.sectionItem">
 						<b :class="$style.musicalNav">NOTICE</b>
-						<img
-							:class="$style.contentImage"
-							alt=""
-							:src="musical?.noticeImage || TestImage"
-						/>
+						<img :class="$style.contentImage" alt="" :src="musical?.noticeImage || TestImage" />
 					</div>
 				</div>
 
 				<div :class="$style.sectionWrapper" ref="detail">
 					<div :class="$style.sectionItem">
 						<b :class="$style.musicalNav">DETAIL</b>
-						<img
-							:class="$style.contentImage"
-							alt=""
-							:src="musical?.detailImage || TestImage"
-						/>
+						<img :class="$style.contentImage" alt="" :src="musical?.detailImage || TestImage" />
 					</div>
 				</div>
 
 				<!-- Additional PLACE section for further details -->
-				<div :class="$style.sectionWrapper">
+				<!-- <div :class="$style.sectionWrapper">
 					<div :class="$style.sectionItem">
 						<b :class="$style.musicalNav">PLACE</b>
-						<img
-							:class="$style.contentImage"
-							alt=""
-							:src="musical?.placeImage || TestImage"
-						/>
+						<img :class="$style.contentImage" alt="" :src="musical?.placeImage || TestImage" />
 					</div>
-				</div>
+				</div> -->
 			</div>
 
 			<!-- Booking button -->
@@ -104,267 +76,294 @@
 </template>
 
 <script>
-	import TestImage from "@/assets/image.png";
-	import CalendarIcon from "@/assets/musical/calendar.svg";
-	import PlaceIcon from "@/assets/musical/place.svg";
-	import TimeIcon from "@/assets/musical/time.svg";
-	import BackHeader from "@/components/BackHeader.vue";
-	import Footer from "@/components/Footer.vue";
-	import { defineComponent } from "vue";
+import TestImage from "@/assets/image.png";
+import CalendarIcon from "@/assets/musical/calendar.svg";
+import PlaceIcon from "@/assets/musical/place.svg";
+import TimeIcon from "@/assets/musical/time.svg";
+import BackHeader from "@/components/BackHeader.vue";
+import Footer from "@/components/Footer.vue";
+import { defineComponent } from "vue";
 
-	export default defineComponent({
-		components: { Footer, BackHeader },
-		data() {
-			return {
-				musical: null,
-				TestImage,
-				PlaceIcon,
-				CalendarIcon,
-				TimeIcon,
-			};
+export default defineComponent({
+	components: { Footer, BackHeader },
+	data() {
+		return {
+			musical: null,
+			TestImage,
+			PlaceIcon,
+			CalendarIcon,
+			TimeIcon,
+		};
+	},
+	methods: {
+		goBack() {
+			this.$router.push("/musical");
 		},
-		methods: {
-			goBack() {
-				this.$router.push("/musical");
-			},
-			redirectToBooking() {
-				const id = this.$route.params.id;
-				this.$router.push(`/booking/${id}/dates`);
-			},
-			async fetchMusical(id) {
-				try {
-					const response = await fetch(
-						`http://localhost:8080/api/musicals/${id}`
-					);
-					if (!response.ok) throw new Error("Network response was not ok");
-					const { result } = await response.json();
-					this.musical = {
-						title: result.title,
-						place: result.place,
-						placeDetail: result.placeDetail,
-						dates: `${result.ticketingStartDate} ~ ${result.ticketingEndDate}`,
-						runningTime: `${result.runningTime}분`,
-						posterImage: result.posterImageUrl || TestImage,
-						noticeImage: result.noticeImageUrl || TestImage,
-						detailImage: result.detailImageUrl || TestImage,
-						placeImage: result.placeImageUrl || TestImage,
-					};
-				} catch (error) {
-					console.error("Failed to fetch data", error);
-				}
-			},
-			scrollToSection(section) {
-				const sectionElement = this.$refs[section];
-				if (sectionElement) {
-					sectionElement.scrollIntoView({ behavior: "smooth" });
-				}
-			},
+		redirectToBooking() {
+			const id = this.$route.params.id;
+			this.$router.push(`/booking/${id}/dates`);
 		},
-		mounted() {
-			const musicalId = this.$route.params.id;
-			this.fetchMusical(musicalId);
+		async fetchMusical(id) {
+			try {
+				const response = await fetch(
+					`http://localhost:8080/api/musicals/${id}`
+				);
+				if (!response.ok) throw new Error("Network response was not ok");
+				const { result } = await response.json();
+				this.musical = {
+					title: result.title,
+					place: result.place,
+					placeDetail: result.placeDetail,
+					dates: `${result.ticketingStartDate} ~ ${result.ticketingEndDate}`,
+					runningTime: `${result.runningTime}분`,
+					posterImage: result.posterImageUrl || TestImage,
+					noticeImage: result.noticeImageUrl || TestImage,
+					detailImage: result.detailImageUrl || TestImage,
+					placeImage: result.placeImageUrl || TestImage,
+				};
+			} catch (error) {
+				console.error("Failed to fetch data", error);
+			}
 		},
-	});
+		scrollToSection(section) {
+			const sectionElement = this.$refs[section];
+			if (sectionElement) {
+				sectionElement.scrollIntoView({ behavior: "smooth" });
+			}
+		},
+	},
+	mounted() {
+		const musicalId = this.$route.params.id;
+		this.fetchMusical(musicalId);
+	},
+});
 </script>
 
 <style module>
-	@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
+.groupParent {
+	width: 100%;
+	height: 50px;
+	/* border: 1px solid blue; */
+	background-color: #fafafa;
+}
 
-	.groupParent {
-		width: 100%;
-		height: 50px;
-		padding: 14px;
-		/* border: 1px solid blue; */
-		background-color: #fafafa;
-	}
+body {
+	margin: 0;
+	line-height: normal;
+}
 
-	body {
-		margin: 0;
-		line-height: normal;
-		font-family: "Roboto", sans-serif;
-	}
-	.navbar {
-		display: flex;
-		justify-content: center;
-		width: 300px;
-		margin-bottom: 10px;
-	}
+.navbar {
+	display: flex;
+	justify-content: center;
+	width: 300px;
+}
 
-	.navbar > p {
-		font-weight: bold;
-		width: 100%;
-		text-align: center;
-		cursor: pointer;
-		padding: 10px;
-		transition: all 0.3s ease;
-	}
+.navbar>p {
+	font-weight: bold;
+	width: 100%;
+	text-align: center;
+	cursor: pointer;
+	padding: 10px;
+	transition: all 0.3s ease;
+}
 
-	.noticeNav {
-		color: grey;
-	}
+.noticeNav {
+	color: grey;
+}
 
-	.detailNav {
-		color: grey;
-	}
+.detailNav {
+	color: grey;
+}
 
-	.active {
-		color: white; /* 활성화된 버튼의 텍스트 색상 */
-		background-color: grey; /* 활성화된 버튼의 배경색 */
-		border-radius: 15px; /* 둥근 모서리 */
-		transition: all 0.3s ease; /* 부드러운 전환 */
-	}
+.active {
+	color: white;
+	/* 활성화된 버튼의 텍스트 색상 */
+	background-color: grey;
+	/* 활성화된 버튼의 배경색 */
+	border-radius: 15px;
+	/* 둥근 모서리 */
+	transition: all 0.3s ease;
+	/* 부드러운 전환 */
+}
 
-	.navbar > p:hover:not(.active) {
-		background-color: lightgray;
-		color: black;
-	}
+.navbar>p:hover:not(.active) {
+	background-color: lightgray;
+	color: black;
+}
 
-	.div {
-		width: 100%;
-		position: relative;
-		background-color: #fafafa;
-		min-height: 100vh;
-		text-align: left;
-		font-size: 16px;
-		color: #c54966;
-		font-family: Roboto;
-	}
-	.wrapper {
-		/* border: 1px solid red; */
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: flex-start; /* 상단 정렬 */
-		width: 100%;
-		height: 100%;
-		background-color: #fafafa;
-		overflow: hidden; /* 추가된 스타일 */
-		/* margin-top: 20px; */
-		padding-top: 20px;
-		position: relative; /* Footer와 버튼을 고정하기 위한 위치 설정 */
-		/** Footer의 높이를 고려하여 최소 높이 설정 */
-		min-height: calc(100vh - 60px);
-	}
-	.wrapper ::-webkit-scrollbar {
-		display: none;
-	}
+.div {
+	width: 100%;
+	position: relative;
+	background-color: #fafafa;
+	min-height: 100vh;
+	text-align: left;
+	font-size: 16px;
+	color: #c54966;
+}
 
-	.content {
-		display: flex;
-		flex-direction: column;
-		justify-content: flex-start; /* 상단 정렬 */
-		align-items: center;
-		max-height: 632px; /* 최대 높이 설정 */
-		overflow-y: auto; /* 스크롤 가능하도록 설정 */
-		flex-grow: 1; /* 남은 공간을 채우도록 설정 */
-	}
+.wrapper {
+	/* border: 1px solid red; */
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: flex-start;
+	/* 상단 정렬 */
+	width: 100%;
+	height: 100%;
+	background-color: #fafafa;
+	overflow: hidden;
+	/* 추가된 스타일 */
+	/* margin-top: 20px; */
+	/* padding-top: 20px; */
+	position: relative;
+	/* Footer와 버튼을 고정하기 위한 위치 설정 */
+	/** Footer의 높이를 고려하여 최소 높이 설정 */
+	/* min-height: calc(100vh - 60px); */
+}
 
-	.posterContainer {
-		position: relative; /* 포스터와 오버레이 상대 위치 */
-		width: 300px; /* 고정 너비 */
-		height: 433px; /* 고정 높이 */
-		display: flex;
-		justify-content: center;
-		margin-bottom: 20px; /* NOTICE와의 간격을 위한 마진 */
-	}
+.wrapper ::-webkit-scrollbar {
+	display: none;
+}
 
-	.posterImage {
-		width: 100%;
-		height: 100%;
-		object-fit: cover;
-		border-radius: 15px; /* 모서리를 둥글게 */
-	}
-	.musicalNav {
-		width: 100%;
-		color: #333;
-	}
-	.overlay {
-		position: absolute; /* 포스터 위에 고정 위치 설정 */
-		bottom: 0;
-		left: 0;
-		width: 100%; /* 부모 요소에 맞춤 */
-		height: 40%;
-		/* 그라데이션 효과 */
-		background: linear-gradient(to top, rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.4));
-		color: white;
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 20px;
-		border-radius: 0 0 15px 15px;
-	}
+.content {
+	/* display: flex; */
+	/* flex-direction: column;
+	justify-content: flex-start; */
+	/* 상단 정렬 */
+	/* align-items: center; */
+	height: 598px;
+	/* 최대 높이 설정 */
+	overflow-y: auto;
+	/* 스크롤 가능하도록 설정 */
+	flex-grow: 1;
+	/* 남은 공간을 채우도록 설정 */
+}
 
-	.details b {
-		font-size: 20px;
-		font-weight: bold;
-		text-align: center;
-	}
+.posterContainer {
+	position: relative;
+	/* 포스터와 오버레이 상대 위치 */
+	width: 300px;
+	/* 고정 너비 */
+	height: 433px;
+	/* 고정 높이 */
+	display: flex;
+	justify-content: center;
+	margin-bottom: 20px;
+	/* NOTICE와의 간격을 위한 마진 */
+}
 
-	.details div {
-		display: flex;
-		align-items: center;
-		margin-bottom: 5px;
-	}
+.posterImage {
+	width: 100%;
+	height: 100%;
+	object-fit: cover;
+	border-radius: 15px;
+	/* 모서리를 둥글게 */
+}
 
-	.details img {
-		width: 20px;
-		height: 20px;
-		margin-right: 8px;
-	}
+.musicalNav {
+	width: 100%;
+	color: #333;
+}
 
-	.section {
-		display: flex;
-		flex-direction: column;
-	}
+.overlay {
+	position: absolute;
+	/* 포스터 위에 고정 위치 설정 */
+	bottom: 0;
+	left: 0;
+	width: 100%;
+	/* 부모 요소에 맞춤 */
+	height: 40%;
+	/* 그라데이션 효과 */
+	background: linear-gradient(to top, rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.4));
+	color: white;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	padding: 20px;
+	border-radius: 0 0 15px 15px;
+}
 
-	.sectionWrapper {
-		width: 300px; /* 고정 너비 */
-	}
+.details b {
+	font-size: 20px;
+	font-weight: bold;
+	text-align: center;
+}
 
-	.sectionItem {
-		display: flex;
-		flex-direction: column; /* 아이템을 세로로 정렬 */
-		align-items: flex-start; /* 왼쪽 정렬 */
-		/* margin-bottom: 20px; 각 항목 간의 간격 */
-		width: 100%;
-	}
+.details div {
+	display: flex;
+	align-items: center;
+	margin-bottom: 5px;
+}
 
-	.contentImage {
-		width: 100%;
-	}
+.details img {
+	width: 20px;
+	height: 20px;
+	margin-right: 8px;
+}
 
-	.sectionItem b {
-		margin-bottom: 10px; /* 텍스트와 이미지 사이의 간격 */
-		font-size: 18px;
-		font-weight: bold; /* 텍스트 굵게 설정 */
-		text-align: left;
-	}
+.section {
+	display: flex;
+	flex-direction: column;
+}
 
-	.noticeText {
-		font-size: 14px;
-		line-height: 1.6;
-		color: #333;
-	}
+.sectionWrapper {
+	width: 300px;
+	/* 고정 너비 */
+}
 
-	.bookingButton {
-		width: 100%;
-		padding: 15px 0;
-		background-color: #c54966; /* 버튼 색상 */
-		color: white;
-		font-size: 18px;
-		font-weight: bold;
-		border: none;
-		border-radius: 15px 15px 0 0;
-		cursor: pointer;
-		text-align: center;
-		position: absolute; /* 고정 위치 설정 */
-		bottom: 58px; /* Footer 바로 위에 위치하도록 설정 */
-	}
+.sectionItem {
+	display: flex;
+	flex-direction: column;
+	/* 아이템을 세로로 정렬 */
+	align-items: flex-start;
+	/* 왼쪽 정렬 */
+	/* margin-bottom: 20px; 각 항목 간의 간격 */
+	width: 100%;
+}
 
-	.footer {
-		position: absolute; /* 고정 위치 설정 */
-		bottom: 0; /* 하단에 위치 */
-		width: 100%; /* 전체 너비 설정 */
-	}
+.contentImage {
+	width: 100%;
+}
+
+.sectionItem b {
+	margin-bottom: 10px;
+	/* 텍스트와 이미지 사이의 간격 */
+	font-size: 18px;
+	font-weight: bold;
+	/* 텍스트 굵게 설정 */
+	text-align: left;
+}
+
+.noticeText {
+	font-size: 14px;
+	line-height: 1.6;
+	color: #333;
+}
+
+.bookingButton {
+	position: fixed;
+	width: 375px;
+	padding: 15px 0;
+	background-color: #c54966;
+	/* 버튼 색상 */
+	color: white;
+	font-size: 18px;
+	font-weight: bold;
+	border: none;
+	border-radius: 15px 15px 0 0;
+	cursor: pointer;
+	text-align: center;
+	bottom: 68px;
+	/* Footer 바로 위에 위치하도록 설정 */
+}
+
+.footer {
+	position: absolute;
+	/* 고정 위치 설정 */
+	bottom: 0;
+	/* 하단에 위치 */
+	width: 100%;
+	/* 전체 너비 설정 */
+}
 </style>
